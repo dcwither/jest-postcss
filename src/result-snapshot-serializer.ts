24693cfa9ede95e78635ca148a19faa139de6f13
Devin Witherspoon <devin.witherspoon@chanzuckerberg.com>
2020-11-04 19:39:41 -0800

    fix up typings

--- src/result-snapshot-serializer.ts
+++ src/result-snapshot-serializer.ts
@@ -4,7 +4,11 @@ import { serializeCSS } from "./utils";
 // string snaphsots should not add the string output
 expect.addSnapshotSerializer({
   test: (val: unknown) => {
-    return val && typeof val === "object" && val.hasOwnProperty("css");
+    return (
+      val &&
+      typeof val === "object" &&
+      Object.prototype.hasOwnProperty.call(val, "css")
+    );
   },
   print: (val: unknown) => {
     return serializeCSS((val as Result).css);
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
 
 import "@types/jest";
+export {};
 
 declare global {
   namespace jest {
-    interface Matchers<R> {
+    interface Matchers<R, T> {
       toMatchCSS(css: string): R;
     }
   }
